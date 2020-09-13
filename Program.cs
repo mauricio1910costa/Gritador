b19b2120b046809781999fa2cdab36b1589c1618
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-12 22:03:22 -0300

    Repedindo a fala em letras maiusculas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome...");
+            string nomeUsuario = Console.ReadLine();
+            Console.Write("Bom dia Sr. ");
+            Console.WriteLine(nomeUsuario.ToUpper());
+            Console.WriteLine("Digite a marca do carro...");
+            string marcaCarro = Console.ReadLine();
+            Console.Write("Não atendemos mais a marca ");
+            Console.WriteLine(marcaCarro.ToUpper());
+            Console.Write("TENHA UM BOM DIA");
+            
+            
+        }
+    }
+}
